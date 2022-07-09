e386977520a325d334a99578e4fd34f961c03bbc
EmotionChild <JDRCaple@outlook.com>
2022-07-09 17:42:49 +1200

    Updated Marmalades module

--- src/Ellie/Modules/Marmalades/Marmalade.cs
+++ src/Ellie/Modules/Marmalades/Marmalade.cs
@@ -3,11 +3,11 @@
 namespace Ellie.Modules;
 
 [OwnerOnly]
-public partial class Medusa : EllieModule<IMarmaladeLoaderService>
+public partial class Marmalade : EllieModule<IMarmaladeLoaderService>
 {
     [Cmd]
     [OwnerOnly]
-    public async partial Task MedusaLoad(string? name = null)
+    public async partial Task MarmaladeLoad(string? name = null)
     {
         if (string.IsNullOrWhiteSpace(name))
         {
@@ -15,14 +15,14 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
                                  .Select(x => x.Name)
                                  .ToHashSet();
             
-            var unloaded = _service.GetAllMedusae()
+            var unloaded = _service.GetAllMarmalades()
                     .Where(x => !loaded.Contains(x))
                     .Select(x => Format.Code(x.ToString()))
                     .ToArray();
 
             if (unloaded.Length == 0)
             {
-                await ReplyPendingLocalizedAsync(strs.no_medusa_available);
+                await ReplyPendingLocalizedAsync(strs.no_marmalade_available);
                 return;
             }
 
@@ -39,17 +39,17 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
             return;
         }
 
-        var res = await _service.LoadMedusaAsync(name);
-        if (res == MedusaLoadResult.Success)
-            await ReplyConfirmLocalizedAsync(strs.medusa_loaded(Format.Code(name)));
+        var res = await _service.LoadMarmaladeAsync(name);
+        if (res == MarmaladeLoadResult.Success)
+            await ReplyConfirmLocalizedAsync(strs.marmalade_loaded(Format.Code(name)));
         else
         {
             var locStr = res switch
             {
-                MedusaLoadResult.Empty => strs.medusa_empty,
-                MedusaLoadResult.AlreadyLoaded => strs.medusa_already_loaded(Format.Code(name)),
-                MedusaLoadResult.NotFound => strs.medusa_invalid_not_found,
-                MedusaLoadResult.UnknownError => strs.error_occured,
+                MarmaladeLoadResult.Empty => strs.marmalade_empty,
+                MarmaladeLoadResult.AlreadyLoaded => strs.marmalade_already_loaded(Format.Code(name)),
+                MarmaladeLoadResult.NotFound => strs.marmalade_invalid_not_found,
+                MarmaladeLoadResult.UnknownError => strs.error_occured,
                 _ => strs.error_occured
             };
 
@@ -59,35 +59,35 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
     
     [Cmd]
     [OwnerOnly]
-    public async partial Task MedusaUnload(string? name = null)
+    public async partial Task MarmaladeUnload(string? name = null)
     {
         if (string.IsNullOrWhiteSpace(name))
         {
-            var loaded = _service.GetLoadedMedusae();
+            var loaded = _service.GetLoadedMarmalades();
             if (loaded.Count == 0)
             {
-                await ReplyPendingLocalizedAsync(strs.no_medusa_loaded);
+                await ReplyPendingLocalizedAsync(strs.no_marmalade_loaded);
                 return;
             }
 
             await ctx.Channel.EmbedAsync(_eb.Create(ctx)
                                             .WithOkColor()
-                                            .WithTitle(GetText(strs.loaded_medusae))
+                                            .WithTitle(GetText(strs.loaded_marmalades))
                                             .WithDescription(loaded.Select(x => x.Name)
                                                                    .Join("\n")));
             
             return;
         }
         
-        var res = await _service.UnloadMedusaAsync(name);
-        if (res == MedusaUnloadResult.Success)
+        var res = await _service.UnloadMarmaladeAsync(name);
+        if (res == MarmaladeUnloadResult.Success)
             await ReplyConfirmLocalizedAsync(strs.medusa_unloaded(Format.Code(name)));
         else
         {
             var locStr = res switch
             {
-                MedusaUnloadResult.NotLoaded => strs.medusa_not_loaded,
-                MedusaUnloadResult.PossiblyUnable => strs.medusa_possibly_cant_unload,
+                MarmaladeUnloadResult.NotLoaded => strs.medusa_not_loaded,
+                MarmaladeUnloadResult.PossiblyUnable => strs.medusa_possibly_cant_unload,
                 _ => strs.error_occured
             };
 
@@ -97,9 +97,9 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
 
     [Cmd]
     [OwnerOnly]
-    public async partial Task MedusaList()
+    public async partial Task MarmaladeList()
     {
-        var all = _service.GetAllMedusae();
+        var all = _service.GetAllMarmalades();
 
         if (all.Count == 0)
         {
@@ -107,7 +107,7 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
             return;
         }
         
-        var loaded = _service.GetLoadedMedusae()
+        var loaded = _service.GetLoadedMarmalades()
                              .Select(x => x.Name)
                              .ToHashSet();
 
@@ -123,7 +123,7 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
         await ctx.SendPaginatedConfirmAsync(0,
             page => _eb.Create(ctx)
                        .WithOkColor()
-                       .WithTitle(GetText(strs.list_of_medusae))
+                       .WithTitle(GetText(strs.list_of_marmalades))
                        .WithDescription(output.Skip(page * 10).Take(10).Join('\n')),
             output.Length,
             10);
@@ -131,13 +131,13 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
 
     [Cmd]
     [OwnerOnly]
-    public async partial Task MedusaInfo(string? name = null)
+    public async partial Task MarmaladeInfo(string? name = null)
     {
-        var medusae = _service.GetLoadedMedusae();
+        var marmalades = _service.GetLoadedMarmalades();
 
         if (name is not null)
         {
-            var found = medusae.FirstOrDefault(x => string.Equals(x.Name,
+            var found = marmalades.FirstOrDefault(x => string.Equals(x.Name,
                 name,
                 StringComparison.InvariantCultureIgnoreCase));
             
@@ -147,8 +147,8 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
                 return;
             }
 
-            var cmdCount = found.Sneks.Sum(x => x.Commands.Count);
-            var cmdNames = found.Sneks
+            var cmdCount = found.Canaries.Sum(x => x.Commands.Count);
+            var cmdNames = found.Canaries
                                 .SelectMany(x => x.Commands)
                                    .Select(x => Format.Code(x.Name))
                                    .Join(" | ");
@@ -158,10 +158,10 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
                         .WithAuthor(GetText(strs.medusa_info))
                         .WithTitle(found.Name)
                         .WithDescription(found.Description)
-                        .AddField(GetText(strs.sneks_count(found.Sneks.Count)),
-                            found.Sneks.Count == 0
+                        .AddField(GetText(strs.canaries_count(found.Canaries.Count)),
+                            found.Canaries.Count == 0
                                 ? "-"
-                                : found.Sneks.Select(x => x.Name).Join('\n'),
+                                : found.Canaries.Select(x => x.Name).Join('\n'),
                             true)
                         .AddField(GetText(strs.commands_count(cmdCount)),
                             string.IsNullOrWhiteSpace(cmdNames)
@@ -173,7 +173,7 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
             return;
         }
 
-        if (medusae.Count == 0)
+        if (marmalades.Count == 0)
         {
             await ReplyPendingLocalizedAsync(strs.no_medusa_loaded);
             return;
@@ -185,16 +185,16 @@ public partial class Medusa : EllieModule<IMarmaladeLoaderService>
                 var eb = _eb.Create(ctx)
                             .WithOkColor();
 
-                foreach (var medusa in medusae.Skip(page * 9).Take(9))
+                foreach (var medusa in marmalades.Skip(page * 9).Take(9))
                 {
                     eb.AddField(medusa.Name,
-                        $@"`Sneks:` {medusa.Sneks.Count}
-`Commands:` {medusa.Sneks.Sum(x => x.Commands.Count)}
+                        $@"`Canaries:` {medusa.Canaries.Count}
+`Commands:` {medusa.Canaries.Sum(x => x.Commands.Count)}
 --
 {medusa.Description}");
                 }
 
                 return eb;
-            }, medusae.Count, 9);
+            }, marmalades.Count, 9);
     }
 }
