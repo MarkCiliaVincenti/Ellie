973f3bd671202d269898818d7b5401f09a8f44ee
EmotionChild <JDRCaple@outlook.com>
2022-08-26 23:47:47 +1200

    Created Ellie.Econ

---
+++ src/Ellie.Econ/Deck/NewCard.cs
@@ -0,0 +1,5 @@
+﻿namespace Ellie.Econ;
+
+public abstract record class NewCard<TSuit, TValue>(TSuit suit, TValue value)
+    where TSuit : struct, Enum
+    where TValue : struct, Enum;
---
+++ src/Ellie.Econ/Deck/Regular/RegularSuit.cs
@@ -0,0 +1,9 @@
+﻿namespace Ellie.Econ;
+
+public enum RegularSuit
+{
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades
+}
