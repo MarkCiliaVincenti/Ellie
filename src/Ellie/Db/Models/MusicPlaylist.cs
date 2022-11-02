2a08a486b6c106d62187a0d4df7acf58ca48c6b3
EmotionChild <emotion@elliebot.net>
2022-11-02 23:28:17 +1300

    Added Db models pt.2
    Signed-off-by: EmotionChild <emotion@elliebot.net>

---
+++ src/Ellie/Db/Models/MusicPlaylist.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+namespace Ellie.Services.Database.Models;
+
+public class MusicPlaylist : DbEntity
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public ulong AuthorId { get; set; }
+    public List<PlaylistSong> Songs { get; set; } = new();
+}
