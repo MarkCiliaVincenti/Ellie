e3992f40eb974b617b9db7e08b59b7c1ba574646
EmotionChild <emotion@elliebot.net>
2022-11-06 18:09:01 +1300

    ED-3 Added common files
    Signed-off-by: EmotionChild <emotion@elliebot.net>

---
+++ src/Ellie/Common/ICloneable.cs
@@ -0,0 +1,8 @@
+﻿#nullable disable
+namespace Ellie.Common;
+
+public interface ICloneable<T>
+    where T : new()
+{
+    public T Clone();
+}
---
+++ src/Ellie/Common/ILogCommandService.cs
@@ -0,0 +1,32 @@
+﻿using Ellie.Services.Database.Models;
+
+namespace Ellie.Common;
+
+public interface ILogCommandService
+{
+    void AddDeleteIgnore(ulong xId);
+    Task LogServer(ulong guildId, ulong channelId, bool actionValue);
+    Task LogIgnore(ulong guildId, ulong itemId, IgnoredItemType itemType);
+    LogSetting? GetGuildLogSettings(ulong guildId);
+    bool Log(ulong guildId, ulong? channelId, LogType type);
+}
+
+public enum LogType
+{
+    Other,
+    MessageUpdated,
+    MessageDeleted,
+    UserJoined,
+    UserLeft,
+    UserBanned,
+    UserUnbanned,
+    UserUpdated,
+    ChannelCreated,
+    ChannelDestroyed,
+    ChannelUpdated,
+    UserPresence,
+    VoicePresence,
+    VoicePresenceTts,
+    UserMuted,
+    UserWarned,
+}
