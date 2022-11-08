c3d1f16b985123e56f7df4da76ea49a2cac13e4d
EmotionChild <emotion@elliebot.net>
2022-11-09 00:37:26 +1300

    ED-2 Added services pt.2
    Signed-off-by: EmotionChild <emotion@elliebot.net>

---
+++ src/Ellie/Services/Impl/StartingGuildsListService.cs
@@ -0,0 +1,18 @@
+﻿#nullable disable
+using System.Collections;
+
+namespace Ellie.Services;
+
+public class StartingGuildsService : IEnumerable<ulong>, IEService
+{
+    private readonly IReadOnlyList<ulong> _guilds;
+
+    public StartingGuildsService(DiscordSocketClient client)
+        => _guilds = client.Guilds.Select(x => x.Id).ToList();
+
+    public IEnumerator<ulong> GetEnumerator()
+        => _guilds.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => _guilds.GetEnumerator();
+}
