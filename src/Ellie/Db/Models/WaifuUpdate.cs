504191441934683de5a951b825190bc0e71d24a9
EmotionChild <emotion@elliebot.net>
2022-11-10 10:30:14 +1300

    ED-10 Added Db files
    Signed-off-by: EmotionChild <emotion@elliebot.net>

---
+++ src/Ellie/Db/Models/WaifuUpdate.cs
@@ -0,0 +1,23 @@
+#nullable disable
+using Ellie.Db.Models;
+
+namespace Ellie.Services.Database.Models;
+
+public class WaifuUpdate : DbEntity
+{
+    public int UserId { get; set; }
+    public DiscordUser User { get; set; }
+    public WaifuUpdateType UpdateType { get; set; }
+
+    public int? OldId { get; set; }
+    public DiscordUser Old { get; set; }
+
+    public int? NewId { get; set; }
+    public DiscordUser New { get; set; }
+}
+
+public enum WaifuUpdateType
+{
+    AffinityChanged,
+    Claimed
+}
---
+++ src/Ellie/Db/Models/WarningPunishment.cs
@@ -0,0 +1,10 @@
+#nullable disable
+namespace Ellie.Services.Database.Models;
+
+public class WarningPunishment : DbEntity
+{
+    public int Count { get; set; }
+    public PunishmentAction Punishment { get; set; }
+    public int Time { get; set; }
+    public ulong? RoleId { get; set; }
+}
