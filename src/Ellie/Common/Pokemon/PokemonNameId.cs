02464d1471b827d75cb06aeb5d1571263d5d7e1b
EmotionChild <emotion@elliebot.net>
2022-11-09 23:46:44 +1300

    ED-9 Added Common files
    Signed-off-by: EmotionChild <emotion@elliebot.net>

---
+++ src/Ellie/Common/Pokemon/PokemonNameId.cs
@@ -0,0 +1,8 @@
+﻿#nullable disable
+namespace Ellie.Common.Pokemon;
+
+public class PokemonNameId
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ src/Ellie/Common/TypeReaders/KwumTypeReader.cs
@@ -0,0 +1,21 @@
+﻿#nullable disable
+using Ellise.Common;
+
+namespace Ellie.Common.TypeReaders;
+
+public sealed class KwumTypeReader : EllieTypeReader<kwum>
+{
+    public override ValueTask<TypeReaderResult<kwum>> ReadAsync(ICommandContext context, string input)
+    {
+        if (kwum.TryParse(input, out var val))
+            return new(TypeReaderResult.FromSuccess(val));
+
+        return new(TypeReaderResult.FromError<kwum>(CommandError.ParseFailed, "Input is not a valid kwum"));
+    }
+}
+
+public sealed class SmartTextTypeReader : EllieTypeReader<SmartText>
+{
+    public override ValueTask<TypeReaderResult<SmartText>> ReadAsync(ICommandContext ctx, string input)
+        => new(TypeReaderResult.FromSuccess(SmartText.CreateFrom(input)));
+}
